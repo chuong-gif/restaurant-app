0353b110c10251e0db1010f0d5f8352c9816c952
Ngô Văn Chương <chuong17102005@gmail.com>
2025-10-09 06:02:35 +0700

    Delete packages/server/src/routes/reservations.ts

--- packages/server/src/routes/reservations.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getReservations, createReservation } from '../controllers/reservationController';
-
-const router = Router();
-
-router.get('/', getReservations);
-router.post('/', createReservation);
-
-export default router;
